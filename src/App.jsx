1c3c8d91c3be54e5c2d736a758da94cfded09c52
AflahAhamed00 <aflahahamed777@gmail.com>
2024-02-16 14:10:19 +0530

    Studied how funtional components in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// how to pass a normal js in to a component?
 
+const title = "This is title";
+
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>How you doing?</h1>
+      <h1>{title}</h1>
     </>
-  )
-}
+  );
+};
 
-export default App
+// how to pass a react component into another component?
+
+const ChildComponent = () => {
+  return <h1>Its a child component</h1>;
+};
+
+const ParentComponent = () => {
+  return (
+    <>
+      <h1>Its a parent component</h1>
+      <ChildComponent />
+    </>
+  );
+};
+
+// how to pass a component into a react element?
+
+const ReactComponent = () => {
+  return <h1>This is a component that is passed into the react element</h1>;
+};
+
+const reactElement = (
+  <>
+    <h1>This is react element</h1>
+    <ReactComponent />
+  </>
+);
+
+export { App, ParentComponent, reactElement };
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {App, ParentComponent, reactElement} from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const root1 = ReactDOM.createRoot(document.getElementById("root1"))
+
+const root2 = ReactDOM.createRoot(document.getElementById("root2"))
+
+root.render(
   <React.StrictMode>
-    <App />
+    < App /> 
   </React.StrictMode>,
+)
+
+root1.render(
+  <React.StrictMode>
+    <ParentComponent />
+  </React.StrictMode>
+)
+
+root2.render(
+  <React.StrictMode>
+    {reactElement}
+  </React.StrictMode>
 )
